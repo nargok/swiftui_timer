7f4469631fd7e7a1d21631f04554ab78b9903ff6
nargok <nargok@gmail.com>
2021-04-25 20:36:20 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by 後閑諒一 on 2021/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
